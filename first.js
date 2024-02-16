cac8593fa861889c20b60235741becb2cea7fabe
AliGultary <muhammadalimhammadi@gmail.com>
2024-02-16 05:24:40 -0800

    added

--- first.js
+++ first.js
@@ -1,6 +1,4 @@
-let variable ="ali";
-var Name= "muhammad";
-const CODE = 1234;
-console.log(CODE);
-console.log(variable);
+let Name;
 console.log(Name);
+
+//hosting is the java script default behaviour of moving decleration to the top of the function or the top of the Global context
