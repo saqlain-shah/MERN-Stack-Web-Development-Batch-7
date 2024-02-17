5517ee1265d53dd42c6fc948ce24bf477094f2ef
PervezHotovi <pervezabbas77@gmail.com>
2024-02-17 23:38:01 +0500

    New lines Added in Object.js file

--- object.js
+++ object.js
@@ -1,8 +1,24 @@
 const person={
-    name : "Pervez",
+    name :{
+        firstName :"Pervez",
+        lastName : "Abbas",
+        surName : "Hotovi"
+    },
     age : 23,
     PhoneNo : 3534
 }
 
+Person1=[
+    "Pervez", "Abbas", 23, 035545
+]
 
-console.log(person)
+console.log("First Name : " + person.name.firstName)   //accessing the specific key value using dot(.) notation
+
+console.log("Last Name : " + person['name']['lastName'])  //accessing the specific key value using bracket notation
+
+console.log("array"  + Person1)
+let num=33;
+let num2=33;
+
+num=num2++; // value will be increment after assigning (num=33 and num2 =34 )
+console.log(num,num2)
