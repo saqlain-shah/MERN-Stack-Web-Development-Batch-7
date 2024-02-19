1a6eac6a08ae8c9cc258206cfcf4c315c2e5a854
Muhammad-Saqlain776 <saqlainjunaid9@gmail.com>
2024-02-19 12:31:32 +0500

    Function

---
+++ FUNCTION.js
@@ -0,0 +1,26 @@
+function myintro(){
+    console.log("iam muhammad saqlain,");
+    console.log('<br>');
+    console.log("i learn javascript,");
+    console.log('<br>');
+    console.log("i want to become a mern stack web developer");
+    
+    
+};
+myintro();
+myintro();
+myintro();
+myintro();
+
+function myName(fname,lname){
+    console.log(fname + ' ' + lname);
+};
+myName('Muhammad','Saqlain');
+
+function sum(x,y){
+    console.log(x+y);
+};
+sum(4,5);
+sum(4,50);
+sum(45,5);
+sum(49,50);
--- saqlain.js
+++ saqlain.js
@@ -1,13 +1,13 @@
-<<<<<<< HEAD
+
 console.log("Hello World")
 console.log("Hello World")
 console.log("Hello World")
 console.log("Hello World")
 console.log("Hello World")
-=======
+
 console.log("new file")
 console.log("new file")
 console.log("new file")
 console.log("new file")
 console.log("new file")
->>>>>>> main
+
