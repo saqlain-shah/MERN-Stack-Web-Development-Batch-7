6e3afdfa0303ffa221b08bee28d1b15a7ad53db4
kamran-aqeel <kamu.jee.1234@gmail.com>
2024-02-21 16:36:03 +0500

    add a new file

---
+++ function2.js
@@ -0,0 +1,15 @@
+function Add  ( ){
+    let a = 18, b =12
+console.log("Add", a+b)
+
+}
+let Add = function(){
+    let a = 90, b =89
+console.log("Add", a+b)
+
+}
+
+let Sub1 = ()=>{
+    let a = 34, b =34
+console.log("Sub1", a-b)
+}
