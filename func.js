ff9c4e7f6d9e456aa934be707b573eb7d54e1b48
shahid-alee <shahidaliabidi10@gmail.com>
2024-02-21 16:30:27 +0500

    add file func.js

---
+++ func.js
@@ -0,0 +1,18 @@
+
+
+let Add = function(){
+    let a = 9, b =8
+console.log("Add", a+b)
+
+}
+function Addition  ( ){
+    let a = 9, b =8
+console.log("Addition", a+b)
+}
+
+
+
+let Sum1 = ()=>{
+    let a = 4, b =1
+console.log("Sum1", a+b)
+}
