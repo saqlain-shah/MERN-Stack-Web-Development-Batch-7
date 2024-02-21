045d15e98fbfda6da88abe811ad3f7fdf58137e6
shahid-alee <shahidaliabidi10@gmail.com>
2024-02-21 16:43:06 +0500

    func.js file

--- func.js
+++ func.js
@@ -5,14 +5,14 @@ let Add = function(){
 console.log("Add", a+b)
 
 }
-function Addition  ( ){
+function subtraction  ( ){
     let a = 9, b =8
-console.log("Addition", a+b)
+console.log("sub", a-b)
 }
 
 
 
-let Sum1 = ()=>{
+let Add = ()=>{
     let a = 4, b =1
-console.log("Sum1", a+b)
+console.log("Add", a+b)
 }
