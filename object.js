df5cf70b262e0cbefae243c886cb609782db6fc6
Iftikhar-Hussain221 <iftikhar27455@gmail.com>
2024-02-21 04:40:55 -0800

    added function.js

---
+++ object.js
@@ -0,0 +1,40 @@
+let stdInfo = {
+   let StdName = {
+    a : "Akhtar Abbas",
+    b : "Baqir",
+    c : "cumming",
+    d : "Dilshair",
+    e : "Enayat",
+    f : "Furqan",
+    g : "Gulzaar",
+    h : "Hidayat",
+    i : "Iftikhar Hussain",
+    j : "Jaffer",
+    k : "Kamran",
+    l : "Liaqat",
+    m : "Muhammad Ali",
+    n : "Noor Ali",
+    o : "Orhan",
+    p : "Pervaiz",
+    q : "Qasim",
+    r : "Rafi",
+    s : "Saqlain Shah",
+    t : "Tanveer",
+    u : "Unus",
+    v : "Virtual",
+    w : "Wilayat",
+    x : "Xam",
+    y : "Yawer",
+    z : "Zakawat"
+};
+ let regNO = {
+    iftikhar : "2332",
+    shahid : "43423",
+    saqlain : "3434",
+    akhtar : "345"
+ };
+
+};
+
+console.log(stdInfo.StdName.i);
+console.log(stdInfo.regNO.iftikhar);
