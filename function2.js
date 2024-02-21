ff080b63ba0322f0fa607aff73c9c5dfd341dcdd
kamran-aqeel <kamu.jee.1234@gmail.com>
2024-02-21 17:00:27 +0500

    changes in function2.js

--- function2.js
+++ function2.js
@@ -1,15 +1,19 @@
-function Add  ( ){
+function Add(){
     let a = 18, b =12
 console.log("Add", a+b)
 
 }
-let Add = function(){
+Add();
+
+let Addition = function(){
     let a = 90, b =89
 console.log("Add", a+b)
 
 }
+Addition();
 
 let Sub1 = ()=>{
     let a = 34, b =34
 console.log("Sub1", a-b)
 }
+Sub1();
