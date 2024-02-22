dd079d182d7dac8f553fae39781896663f028736
AliGultary <muhammadalimhammadi@gmail.com>
2024-02-22 05:13:43 -0800

    added array and object file

---
+++ operators.js
@@ -0,0 +1,24 @@
+let a=10;
+let b=35;
+ 
+
+// if (!(a==b) && !(b<a))
+// {
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+
+// }
+
+// moduls
+
+let result =b%a;
+console.log(result);
+
+
+
+if (!(num1== num2 && !num1== num2)){
+console.log("true")
+}else
+console.log("false")
