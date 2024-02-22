853e845658ed6405ba83066e04e954d075d44f95
mali387 <ma9614581@gmail.com>
2024-02-22 16:23:04 +0500

    solve conflict

--- condition.js
+++ condition.js
@@ -6,4 +6,26 @@ function show2(){
     console.log("M ALI 387");
         
     }
-if(5==5 ? Show():show2());
+if(5==4 ? Show():show2());
+var a=4;
+var b="ali";
+
+
+console.log(b*a);
+switch(a){
+    case 1:
+        console.log("pervaz1");
+        break;
+        case 2:
+        console.log("pervaz2");
+        break;
+        case 3:
+        console.log("pervaz3");
+        break;
+        case 4:
+        console.log("pervaz4");
+        break;
+        default:
+        console.log("Sakhawat");
+        break;
+}
