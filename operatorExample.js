48c30fda05018c98e3953219b9692c53b743b234
PervezHotovi <pervezabbas77@gmail.com>
2024-02-22 17:40:15 +0500

    Files added

---
+++ operatorExample.js
@@ -0,0 +1,18 @@
+let num1 = 5; 
+let num2 =5;
+
+if (!(num1== num2 && !num1== num2)){
+console.log("true")
+}else
+console.log("false")
+
+if(num1>num2 || num1==num2){
+    console.log("true")
+}else{
+    console.log("false")
+}
+if (num1 === num2) 
+{   console.log("true")
+}else{
+    console.log("false")
+}
