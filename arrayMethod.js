dd8e0e9d78f4258e7f58b3f35b22d4250e5f6391
AliGultary <muhammadalimhammadi@gmail.com>
2024-02-28 05:52:01 -0800

    today topic

---
+++ arrayMethod.js
@@ -0,0 +1,3 @@
+// in this we will cover array method
+//1:isArray,length,push.pop,shift ,unshift,foreach,
+// Object:method ()
---
+++ stringMethod.js
@@ -0,0 +1,12 @@
+
+let string ="sring";
+let newSting =string.toUpperCase(string);
+console.log(newSting);
+let upperCase =string.toLowerCase(newSting);
+console.log("to lower case",upperCase);
+
+
+
+
+
+
