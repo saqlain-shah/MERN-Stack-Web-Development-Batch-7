c460500448285a7092c911d0fc2a0209c569f6f8
mali387 <ma9614581@gmail.com>
2024-03-07 16:26:42 +0500

    added

---
+++ callBack.js
@@ -0,0 +1,13 @@
+function myDisplayer(a){
+    console.log(a);
+}
+function myFirst() {
+    myDisplayer("Hello");
+  }
+  
+  function mySecond() {
+    myDisplayer("Goodbye");
+  }
+  
+  myFirst();
+  mySecond();
