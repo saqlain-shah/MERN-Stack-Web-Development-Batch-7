20ec9a065ed87e84bc699bd56a04878e9fe6a607
AliGultary <muhammadalimhammadi@gmail.com>
2024-03-08 05:34:24 -0800

    define the topic

--- asyncWait.js
+++ asyncWait.js
@@ -1,1 +1,4 @@
 // async wait :
+// async is a asynchronous method that can be used in js to run the code asynchronously 
+// it will not block the execution of code 
+// as the js behavour is synchronous and single threading so to over come this problem we use async wait keyword
