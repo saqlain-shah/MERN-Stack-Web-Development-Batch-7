4ca62749aeb6a221404cfb454e5cb64b37a6e8ff
AliGultary <muhammadalimhammadi@gmail.com>
2024-03-12 09:29:46 -0700

    react app

---
+++ react/my-react-app/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <h1>welcome muhammad ali </h1>
+   
+    </>
+  )
+}
+
+export default App
