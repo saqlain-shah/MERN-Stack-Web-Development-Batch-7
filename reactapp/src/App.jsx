ec0be843d6fd430de17b62602eec1594e62c0025
PervezHotovi <pervezabbas77@gmail.com>
2024-03-15 21:23:02 +0500

    Updated reactapp file

---
+++ reactapp/src/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+import React from 'react';
+import UserDataForm from './components/firstComponent.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <h1>User Data Form</h1>
+      <UserDataForm />
+    </div>
+  );
+};
+
+export default App;
